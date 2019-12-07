3f3ba43f26bf4855ef62dbd825046bd0d1e597cd
Daniil Strasser <dajka444@gmail.com>
2019-12-07 23:09:32 +0800

    error

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/bin/sh node
+import nameHello from '../index';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+nameHello();
