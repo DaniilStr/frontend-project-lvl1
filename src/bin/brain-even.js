c1354cd248f0611671b40ecdb596b4c009ba3b3a
Daniil Strasser <dajka444@gmail.com>
2019-12-19 14:59:45 +0800

    game written

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,7 +1,5 @@
 #!/bin/sh node
-import { userName, question } from '../index';
+import game from '../index';
 
 console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-userName();
-question();
+game();
